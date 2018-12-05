1a321287c9f43ceffa85165104d8c0627e50a846
7korobi <7korobi@BA-1810001-MAC.local>
2018-12-05 18:48:58 +0900

    スナップショットテスティング解説。

--- proj/__tests__/lib1_spec.js
+++ proj/__tests__/lib1_spec.js
@@ -1,6 +1,9 @@
+// テスト対象をimportする。
 import lib from '../src/js/lib1';
 
+// describe は、個々のtestを取りまとめるために使う。
 describe("lib1", ()=> {
+  // testにはそれぞれタイトルとシナリオfunctionを渡す。シナリオは非同期実行される。
   test('snapshot', ()=> {
     expect(
       lib(process)
