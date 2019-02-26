251383283eb123a23a5568984926125d6e41b2cf
7korobi <7korobi@BA-1810001-MAC.local>
2019-02-26 15:21:13 +0900

    cleanup.

--- next/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  verbose: true,
-  testURL: "http://localhost/",
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
-}
